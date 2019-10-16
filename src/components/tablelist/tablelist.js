f7f2d861040e1615e7f66337dbbcd0482b8d2b77
tannuxie <x94234@gmail.com>
2019-10-16 12:49:33 +0200

    📦 NEW: flyttat om så det är en dedikerad sävsjö-sida och fixat (förhoppningsvis) filtrering av företag :D

---
+++ src/components/tablelist/tablelist.js
@@ -0,0 +1,177 @@
+import React from 'react';
+import '~style/style.scss';
+import { css } from "@emotion/core"
+import ReactTable from 'react-table'
+import { ReactTableDefaults } from 'react-table'
+import { Link, graphql } from 'gatsby'
+import Img from 'gatsby-image'
+import 'react-table/react-table.css'
+import Layout from '~components/layout/layout';
+
+const TableList = ({ data }) => { 
+    {const array = data.company.edges.map((items, i, data) => {
+        let cObj = {
+            id: 'id',
+            companyquality: 'companyquality',
+            type: 'type',
+            name: 'name',
+            strapiId: 'strapiId',
+            summary: 'summary',
+            imageId: 'imageId',
+            used: [{
+                id: 'id',
+                address: 'address',
+                startdate: 'startdate',
+                enddate: 'enddate',
+                addressstring1: 'addressstring1',
+                addressstring1: 'addressstring1',
+                cityId: 'cityId'
+                }]
+        };
+        cObj.id = items.node.id;
+        cObj.companyquality = items.node.companyquality;
+        cObj.type = items.node.type;
+        cObj.name = items.node.name;
+        cObj.strapiId = items.node.strapiId;
+        cObj.summary = items.node.summary;
+        cObj.imageId = items.node.coverimage.imagecontent.id;
+        cObj.used = items.node.usedaddresses.map((items, i) => {
+            let cUsed = {
+                id: 'id',
+                address: 'address',
+                startdate: 'startdate',
+                enddate: 'enddate',
+                addressstring1: 'addressstring1',
+                addressstring1: 'addressstring1',
+                cityId: 'cityId'
+            }
+            cUsed.id = items.id;
+            cUsed.address = items.address;
+            cUsed.startdate = items.startdate;
+            cUsed.enddate = items.enddate;
+
+            return cUsed;
+        })
+    
+        return {
+            value: cObj
+        };
+    })
+
+    data.address.edges.forEach(function(item) {
+        array.forEach(function(arrayitem) {
+            arrayitem.value.used.forEach(function(useditem) {
+                if(item.node.strapiId === useditem.address) {
+                    useditem.addressstring1 = item.node.addressstring1;
+                    useditem.addressstring2 = item.node.addressstring2;
+                    useditem.cityId = item.node.city.id;
+                }
+            })
+        })
+    });
+
+    const columns = [{
+        id: 'compImage',
+        Header: '',
+        style: {
+            flexGrow: '50'
+        },
+        sortable: false,
+        filterable: false,
+        accessor: 'value.imageId',
+        Cell: row => {
+            let foundObject = {};
+            {data.image.edges.forEach(function(item) {
+                if(item.node.strapiId === row.value) {
+                    foundObject = item.node;
+                };
+            })}
+            return (
+                <div css={css`max-height:500px;overflow:hidden;`}>
+                    <Img 
+                        fluid={foundObject.imagecontent.childImageSharp.fluid} 
+                        alt={foundObject.title} 
+                        title={foundObject.companycoverimage.name}
+                    />
+                </div>
+            );                        
+        }
+    }, {
+        id: 'compMain',
+        Header: '',
+        style: {
+            flexGrow: '200'
+        },
+        sortable: false,
+        filterable: false,
+        accessor: 'value',
+        Cell: row => {
+            return (
+                <div>
+                    <h3>{row.value.name}</h3>
+                    <p style={{textTransform: 'capitalize'}}>{row.value.type}</p>
+                    <p css={css`white-space: pre-wrap;`}>{row.value.summary}</p>
+                </div>
+            )
+        }
+    }, 
+    {
+        id: 'compName',
+        Header: 'Sök på namn',
+        accessor: 'value.name',
+        className: 'nameClass',
+        style: {
+            display: 'none'
+        },
+        Cell: row => {
+            return null
+        }
+    },
+    {
+        id: 'compQual',
+        Header: 'Quality',
+        accessor: 'value.companyquality',
+        show: false,
+    }, {
+        id: 'compType',
+        Header: 'type',
+        accessor: 'value.type',
+        show: false,
+    }, 
+    {
+        id: 'compSummary',
+        Header: 'summary',
+        accessor: 'value.summary',
+        show: false,
+
+    }] 
+
+
+    return (
+        <Layout>
+            <ReactTable
+                data={array}
+                columns={columns}
+                defaultPageSize={10}
+                className={'table'}
+                sorted={[{ // the sorting model for the table
+                    id: 'compQual',
+                    desc: true
+                  }, {
+                    id: 'compName',
+                    desc: true
+                }]}
+                filterable={true}
+                previousText={'Föregående'}
+                nextText={'Nästa'}
+                loadingText={'Laddar...'}
+                noDataText={'Här fanns det inget!'}
+                pageText={'Sida'}
+                ofText={'av'}
+                rowsText={'rader'}
+            />
+        </Layout>
+    )}
+};
+
+export default TableList;
---
+++ src/pages/savsjo.js
@@ -0,0 +1,189 @@
+import React from 'react';
+import ReactTable from 'react-table'
+import { ReactTableDefaults } from 'react-table'
+import { Link, graphql } from 'gatsby'
+import Img from 'gatsby-image'
+import { css } from "@emotion/core"
+import 'react-table/react-table.css'
+import Layout from '~components/layout/layout';
+import TableList from '~components/tablelist/tablelist'
+
+const CITYCONST = 'Sävsjö'
+
+const SavsjoPage = ({ data }) => {
+    console.log(data)
+    let thisCity = {};
+    data.city.edges.forEach(element => {
+        if (element.node.name == CITYCONST) {
+            thisCity = element.node
+            console.log('correct: ' + thisCity.name + ' ' + thisCity.id);
+            
+        }
+    });
+    console.log('company array length: ' + data.company.edges.length);
+    
+    for (let index = data.company.edges.length -1; index >= 0; index--) {
+        let current = data.company.edges[index].node;
+        console.log('current: ' + index + ' ' + JSON.stringify(current));
+        console.log('current typeof: ' + typeof current.usedaddresses);
+        
+        let lastAddress = {};
+        if(typeof current.usedaddresses[0] !== "undefined") {
+            lastAddress = current.usedaddresses[0];
+            console.log('currently in if: ' + index + ' ' + current);
+            
+            if(typeof lastAddress !== "undefined") {
+                console.log('date parse: ' + lastAddress.enddate + ' into: ' + Date.parse(lastAddress.enddate));
+            }
+            console.log('used length: ' + current.usedaddresses.length);
+            
+            for (let index2 = 0; index2 < current.usedaddresses.length; index2++) {
+                let current2 = current.usedaddresses[index2];
+                if (current2.enddate == null) {
+                    lastAddress = current2;
+                    console.log(JSON.stringify(lastAddress) + ' is the latest');
+                    
+                    break;
+                } else if (Date.parse(lastAddress.enddate) < Date.parse(current2.enddate)) {
+                    lastAddress = current2;
+                    console.log(Date.parse(lastAddress.enddate) + ' is older than ' + Date.parse(current2.enddate));
+                    
+                }            
+            }
+        }
+        console.log('latest address: ' + JSON.stringify(lastAddress))
+        //nu har jag senaste used, hitta nu address, 
+        //för att därifrån hitta cityID
+        let found = false;
+        for (let index3 = 0; index3 < data.address.edges.length; index3++) {
+            console.log('testing if address id matches lastAddress id: ' + data.address.edges[index3].node.strapiId + ' + ' + lastAddress.address);
+            console.log(data.address.edges[index3].node.strapiId === lastAddress.address);
+            
+            //const element = data.address.edges[index2].node;
+            if(data.address.edges[index3].node.strapiId === lastAddress.address) {
+                console.log(data.address.edges[index3].node.city.name + ', ' + data.address.edges[index3].node.city.id + '+' + thisCity.strapiId);
+                console.log(data.address.edges[index3].node.city.id !== thisCity.strapiId);
+                if (data.address.edges[index3].node.city.name !== CITYCONST) {
+                    //ta bort, splicea bort detta företaget
+                    found = true;
+                    console.log('inte i sävsjö, tar bort');
+                    
+                    data.company.edges.splice(index, 1);
+                    break;
+                } else if (data.address.edges[index3].node.city.name == CITYCONST) {
+                    found = true;
+                    console.log('är i sävsjö, tar inte bort');
+                    break;
+                }
+            }
+        }
+        if (found == false) {
+            console.log('hittades ingen matchande city, tar bort');
+            data.company.edges.splice(index, 1);
+        }
+        console.log('finished array length: ' + data.company.edges.length);
+        
+
+    }
+    return (
+    <TableList data={data} />
+    )
+};
+
+export default SavsjoPage;
+
+export const query = graphql`
+    query CompanyQuery {
+        company: allStrapiCompany {
+            edges {
+                node {
+                    id
+                    companyquality
+                    type
+                    name
+                    strapiId
+                    summary
+                    usedaddresses {
+                        id
+                        address
+                        startdate
+                        enddate
+                    }
+                    coverimage {
+                        imagecontent {
+                            id
+                        }
+                        
+                    }
+                }
+            }
+        }
+        address: allStrapiAddress {
+            edges {
+                node {
+                    id
+                    strapiId
+                    longitude
+                    latitude
+                    addressstring1
+                    addressstring2
+                    city {
+                        id
+                        name
+                    }
+                    usedaddresses {
+                        id
+                        companyaddress
+                        address
+                        startdate
+                        enddate
+                    }
+                }
+            }
+        }
+        image: allStrapiImage {
+            edges {
+                node {
+                    id
+                    title
+                    strapiId
+                    imagecontent {
+                        childImageSharp {
+                            fluid(maxWidth: 970) {
+                                ...GatsbyImageSharpFluid
+                            }
+                        }
+                    }
+                    companycoverimage {
+                        name
+                        id
+                        summary
+                    }
+                }
+            }
+        }
+        city: allStrapiCity {
+            edges {
+                node {
+                    id
+                    longitude
+                    latitude
+                    children {
+                        id
+                    }
+                    name
+                    parent {
+                        id
+                    }
+                    strapiId
+                    zoom
+                    updated_at
+                    streets {
+                        id
+                        name
+                    }
+                }
+            }
+        }
+    }
+` 

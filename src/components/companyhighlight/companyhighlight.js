e0c486573ec0b68c863513918db27e494d114ce1
tannuxie <x94234@gmail.com>
2020-01-21 13:01:56 +0100

    lagt till en ny komponent som visar slumpat företag

---
+++ src/components/companyhighlight/companyhighlight.js
@@ -0,0 +1,157 @@
+import React from 'react';
+import { css } from '@emotion/core';
+import { Link, StaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import { rhythm, scale } from '../../style/typography';
+
+const CompanyHighlight = ({ data, filtering = 1 }) => {
+    console.log('CompanyHighlight data', data);
+    const filterValue = (filtering !== 1 && filtering !== 2 && filtering !== 3) ? 1 : filtering;
+
+    console.log('CompanyHighlight filtering', filterValue);
+    const companies = data.allStrapiCompany.edges.map((item) => item.node);
+    console.log('CompanyHighlight companies', companies);
+
+    const filteredCompanies = React.useMemo(() => (
+        () => {
+            const filtered = companies.filter((item) => item.quality >= filterValue);
+            if (filtered.length === 0) {
+                return companies;
+            }
+                return filtered;
+        }
+    )(), [filterValue]);
+    console.log(filteredCompanies);
+
+    const randomCompany = filteredCompanies[Math.floor(Math.random() * filteredCompanies.length)];
+    console.log('randomCompany', randomCompany);
+
+    return (
+        <>
+            <div
+                className='column is-4'
+                css={css`
+                max-height:500px;
+                overflow:hidden;
+                display:flex;
+                flex-grow:1;
+                @media (max-width: 769px) {
+                    max-height:300px;
+                }
+                `}
+            >
+                <Img
+                    fluid={randomCompany.mainimage.childImageSharp.fluid}
+                    alt={randomCompany.name}
+                    title={randomCompany.name}
+                />
+            </div>
+
+            <div
+                className='column'
+                css={css`
+                    padding-left: calc(${rhythm} / 2);
+                `}
+            >
+                <Link to={`/industri/${randomCompany.fields.slug}`}>
+                    <h3
+                        css={css`
+                            white-space: normal;
+                            margin-top: calc(${rhythm} / 2);
+                            padding-right: calc(${rhythm} / 2);
+                            @media (max-width: 769px) {
+                                margin-top:0px;
+                            }
+                        `}
+                    >
+                        {randomCompany.name}
+                    </h3>
+                </Link>
+                <p
+                    css={css`
+                        text-transform: capitalize;
+                        font-style: italic;
+                        margin-bottom: 0.125rem;
+                    `}
+                >
+                    {randomCompany.city}
+                    {' - '}
+                    {randomCompany.type}
+                </p>
+                <p
+                    css={css`
+                        white-space: pre-wrap;
+                        margin-bottom: 0;
+                        position: relative;
+                        padding-right: calc(${rhythm} / 2);
+                        max-height: calc(${rhythm} * 8);
+                        overflow: hidden;
+
+                        :before {
+                            content: "...";
+                            position: absolute;
+                            bottom: 0;
+                            right: 0;
+                            width: 1rem;
+                        }
+                        :after {
+                            content: "";
+                            position: absolute;
+                            right: 0;
+                            width: 1rem;
+                            height: ${rhythm};
+                            background: white;
+                        }
+                    `}
+                >
+                    {randomCompany.summary}
+                </p>
+            </div>
+        </>
+    );
+};
+
+export default (props) => (
+	<StaticQuery
+		query={graphql`
+            query HighlightQuery {
+                allStrapiCompany(filter: {published: {eq: true}}) {
+                    edges {
+                        node {
+                            id
+                            strapiId
+                            name
+                            published
+                            quality
+                            summary
+                            type
+                            fields {
+                                slug
+                            }
+                            city
+                            address {
+                                addresstext1
+                                addresstext2
+                                enddate
+                                id
+                                latitude
+                                longitude
+                                startdate
+                            }
+                            mainimage {
+                                childImageSharp {
+                                    fluid {
+                                        src
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+		`}
+		render={(data) => (
+			<CompanyHighlight data={data} {...props} />
+		)}
+	/>
+);

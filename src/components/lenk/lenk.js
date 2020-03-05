286873881142d2a0417960b2cf23ad8f00c1b556
tannuxie <x94234@gmail.com>
2020-03-05 11:21:47 +0100

    börjat bygga om lenk-komponenten och lägga till en ljudspelarfunktion

--- src/components/lenk/lenk.js
+++ src/components/lenk/lenk.js
@@ -3,6 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { css } from '@emotion/core';
 import { Link, StaticQuery, graphql } from 'gatsby';
+import ReactAudioPlayer from 'react-audio-player';
 
 const Lenk = ({
     data, id, type, children,
@@ -13,14 +14,45 @@ const Lenk = ({
     console.log('data: ', data);
 
     let theLink = null;
+
+    switch (type) {
+        case 'article' || 'artikel': {
+            const object = data.allStrapiArticle.edges.find((item) => {
+                console.log('article item: ', item, 'theId: ', id);
+                return item.node.strapiId === Number(id) && item.node.published === true;
+            });
+            console.log('article object: ', object);
+            if (object) {
+                theLink = (
+                    <Link
+                        to={`/${searchObject.type}/${searchObject.slug}`}
+                    >
+                        {children}
+                    </Link>
+                );
+            }
+            break;
+        }
+        case 'company' || 'företag' || 'foretag': {
+
+            break;
+        }
+        case 'sound' || 'ljud' || 'ljudfil': {
+
+            break;
+        }
+        default:
+            break;
+    }
+
     function getLink(theId, theType) {
         console.log('in getLink');
         console.log('id: ', theId, 'theType: ', theType);
 
         function isArticle() {
-            const object = data.allStrapiArticle.edges.find((current) => {
-                console.log('article current: ', current, 'theId: ', theId);
-                return current.node.strapiId === Number(theId) && current.node.published === true;
+            const object = data.allStrapiArticle.edges.find((item) => {
+                console.log('article item: ', item, 'theId: ', theId);
+                return item.node.strapiId === Number(theId) && item.node.published === true;
             });
             console.log('article object: ', object);
 
@@ -31,9 +63,9 @@ const Lenk = ({
             } : undefined;
         }
         function isCompany() {
-            const object = data.allStrapiCompany.edges.find((current) => {
-                console.log('company current: ', current, 'theId: ', theId);
-                return current.node.strapiId === Number(theId) && current.node.published === true;
+            const object = data.allStrapiCompany.edges.find((item) => {
+                console.log('company item: ', item, 'theId: ', theId);
+                return item.node.strapiId === Number(theId) && item.node.published === true;
             });
             console.log('company object: ', object);
 
@@ -92,6 +124,23 @@ export default (props) => (
                             strapiId
                             title
                             published
+                        }
+                    }
+                }
+                allStrapiMedia {
+                    edges {
+                        node {
+                            id
+                            name
+                            strapiId
+                            media {
+                                base
+                                extension
+                                name
+                                prettySize
+                                publicURL
+                                size
+                            }
                         }
                     }
                 }
